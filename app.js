42a7f5e81634bee6fb1612984eed1e6fb0823aae
eunjoo0311 <yyy7423@naver.com>
2023-04-24 23:36:44 +0900

    Add:완성본

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const textarea = document.querySelector("textarea");
 const addBtn = document.getElementById("addBtn");
 const list = document.getElementById("list");
-const delBtn = document.getElementById("delBtn");
+
+// localStorage에 저장된 데이터가 있는지 확인하고, 있으면 리스트를 생성
+if (localStorage.getItem("memo")) {
+  list.innerHTML = localStorage.getItem("memo");
+
+  // 삭제
+  const delBtns = list.querySelectorAll("#delBtn");
+  delBtns.forEach((btn) => {
+    btn.addEventListener("click", function (event) {
+      event.target.parentNode.remove();
+      localStorage.setItem("memo", list.innerHTML);
+    });
+  });
+}
 
 addBtn.addEventListener("click", function (event) {
   const memoList = document.createElement("div");
   memoList.innerHTML = textarea.value;
   memoList.innerHTML += `<button id='delBtn' type='button' >삭제</button>`;
-  list.appendChild(memoList);
+  list.prepend(memoList);
   textarea.value = "";
 
-  list.addEventListener("click", function (event) {
-    if (event.target.id === "delBtn") {
-      event.target.parentNode.remove();
-    }
+  localStorage.setItem("memo", list.innerHTML);
+
+  const delBtn = memoList.querySelector("#delBtn");
+  delBtn.addEventListener("click", function (event) {
+    event.target.parentNode.remove();
+    localStorage.setItem("memo", list.innerHTML);
   });
 });
